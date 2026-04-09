625e10e22e9028d1eed8ed20e3a47509ad5acb07
yxuru <haynescamden@gmail.com>
2026-04-09 15:55:05 -0400

    important changes

--- src/main/java/net/yxuru/tutorialmod/Config.java
+++ src/main/java/net/yxuru/tutorialmod/Config.java
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
+
+// very important stuff
 @Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config
 {
